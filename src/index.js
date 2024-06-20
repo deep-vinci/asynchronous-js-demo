dec480ffc1168e6d53f83b99de23b06a375db0d4
Deepak <deepakworkpc@gmail.com>
2024-06-20 18:33:19 +0530

    adding appropriate code block

--- src/index.js
+++ src/index.js
@@ -9,23 +9,58 @@ hljs.registerLanguage('javascript', javascript);
 
 const codeBlocks = document.querySelectorAll("pre");
 
-const callback = `var fs = require('fs')
+const callback = `console.log(1)
+
 fs.readFile('config.txt', finishedReading)
 
-function finishedReading(error, data) {
+const finishedReading = (error, data) => {
     // handle error here
     console.log(data)
 }
+
+console.log(2)
+`;
+
+const promises = `console.log(1)
+
+let readConfigFile = new Promise(function(resolve, reject) {
+
+    // read the file
+
+    if(file) {
+        resolve('Success!');
+    }
+    else {
+        reject('Failure!');
+    }
+});
+
+p.then(result => { 
+    console.log(result)
+}).catch(err => {
+    console.erorr(err)
+}).finally(function() {
+   console.log("Process Done!")
+});
+
+console.log(2)
+`
+
+const asyncAwait = `console.log(1)
+
+import { promises as fs } from "fs";
+
+const readConfigFile = async () => {
+    const result = await readFile('config.txt','binary')
+    console.log(result)
+}
+console.log(2)
 `
 
 const codeSnippets = {
   callback,
-  promises: `function helloWorld() {
-    console.log('promises');
-  }`,
-  asyncAwait: `function helloWorld() {
-    console.log('async and await');
-  }`
+  promises,
+  asyncAwait
 };
 
 
